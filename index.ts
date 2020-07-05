8c5c41907d4aeacd4871572cfc7abd00fcd97b24
Alan Harper <475+aussiegeek@users.noreply.github.com>
2020-07-05 18:16:00 +1000

    Setup apollo server (#8)

--- index.ts
+++ index.ts
@@ -1,18 +1,7 @@
-import express from "express";
-import graphqlHTTP from "express-graphql";
-import schema from "./schema";
-
+import server from "./server";
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-app.use(
-  "/",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
-);
-
-app.listen(PORT, () => {
-  console.info(`Listening on http://localhost:${PORT}/`);
+// The `listen` method launches a web server.
+server.listen({ port: PORT }).then(({ url }) => {
+  console.info(`Listening on ${url}`);
 });
