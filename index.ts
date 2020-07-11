7be4b23fb7cdf1c9a0480850fb2ff04ce49f2bde
Alan Harper <475+aussiegeek@users.noreply.github.com>
2020-07-11 12:31:06 +1000

    Setup netlify functions

--- index.ts
+++
@@ -1,7 +0,0 @@
-import server from "./server";
-const PORT = process.env.PORT || 3000;
-
-// The `listen` method launches a web server.
-server.listen({ port: PORT }).then(({ url }) => {
-  console.info(`Listening on ${url}`);
-});
