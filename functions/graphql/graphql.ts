d70ab38300713f5342972f319fb379e4e426b312
Alan Harper <475+aussiegeek@users.noreply.github.com>
2020-07-15 22:42:25 +1000

    Setup very basic honeycomb integration (#21)

--- functions/graphql/graphql.ts
+++ functions/graphql/graphql.ts
@@ -1,7 +1,14 @@
 import fs from "fs";
 import path from "path";
 import { ApolloServer, gql } from "apollo-server-lambda";
+import { Handler } from "aws-lambda";
 import resolvers from "../../resolvers";
+
+const Libhoney = require("libhoney");
+let hny = new Libhoney({
+  writeKey: process.env["HONEY_WRITE_KEY"],
+  dataset: "graphqlcountries",
+});
 
 const findSchema = (dir: string, depth?: number): string => {
   if (!depth) {
@@ -29,4 +36,21 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-export const handler = server.createHandler();
+const apolloHandler = server.createHandler();
+
+export const handler: Handler = (evt, context, callback) => {
+  let startTime = Date.now();
+  let ev = hny.newEvent();
+  ev.add({ durationMs: Date.now() - startTime });
+
+  apolloHandler(evt, context, (error, result) => {
+    if (result) {
+      ev.add({ "response.statusCode": result.statusCode });
+    }
+    if (error) {
+      ev.add({ error });
+    }
+    ev.send();
+    callback(error, result);
+  });
+};
